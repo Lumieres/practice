b9ed40fcd30d75eb62dda93d8770cd2e03e19862
lumieres <liosho1535@gmail.com>
2024-04-05 06:13:13 +0900

    delete hello_world & PorjectApp model & Add Icon

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,29 +10,16 @@ from django.views.generic import CreateView, DetailView, DeleteView
 from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorator import account_ownership_required
-from accountapp.models import HelloWorld
+
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 # Create your views here.
-@login_required
-def hello_world(request):
-    if(request.method == "POST"):
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld();
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={"hello_world_list": hello_world_list})
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -51,7 +38,7 @@ class AccountDetailView(DetailView, MultipleObjectMixin):
 class AccountUpdateView(PasswordChangeView):
     model = User
     context_object_name = "target_user"
-    success_url = reverse_lazy("accountapp:hello_world")
+    success_url = reverse_lazy("home")
     template_name = "accountapp/update.html"
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
